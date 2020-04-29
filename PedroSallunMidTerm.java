8ef36d62152b4dd50b825e9d0878195d29f59fbf
pedrosallun <64516204+pedrosallun@users.noreply.github.com>
2020-04-29 15:30:48 -0600

    Add files via upload

---
+++ PedroSallunMidTerm.java
@@ -0,0 +1,15 @@
+// Pedro Sallun
+public class PedroSallunMidTerm
+{ 
+    public static void main(String[] args) 
+    { 
+    double a = 237.21;
+    double b = 321.65;
+    double c = a * b;
+        System.out.println("*******************");
+        System.out.println("*\t\t\t*\t\t\t*");
+        System.out.println("*\t\t\t*\t\t\t*");
+        System.out.println("*******************");
+        System.out.printf("%.4f times %.4f equals %.4f" , a, b, c);
+    }
+}
